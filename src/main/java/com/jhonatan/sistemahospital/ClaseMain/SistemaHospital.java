5052f42b0378c623d01aece41495847299c07fad
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-07-21 14:24:11 -0500

    clase main: llamando al formulario principal

---
+++ src/main/java/com/jhonatan/sistemahospital/ClaseMain/SistemaHospital.java
@@ -0,0 +1,12 @@
+package com.jhonatan.sistemahospital.ClaseMain;
+
+import com.jhonatan.sistemahospital.Igu.Dashboard;
+
+public class SistemaHospital {
+
+    public static void main(String[] args) {
+        Dashboard d = new Dashboard();
+        d.setVisible(true);
+        
+    }
+}
