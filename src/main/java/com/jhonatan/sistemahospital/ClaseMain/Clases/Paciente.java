de5ebf78ad4b62e8ef731292fb24ed47edb77185
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-07-22 18:11:14 -0500

    class Paciente: atributos de esta clase

--- src/main/java/com/jhonatan/sistemahospital/ClaseMain/Clases/Paciente.java
+++ src/main/java/com/jhonatan/sistemahospital/ClaseMain/Clases/Paciente.java
@@ -1,5 +1,21 @@
 package com.jhonatan.sistemahospital.ClaseMain.Clases;
 
+import java.util.Date;
+
+
 public class Paciente {
- 
+    //atributos 
+    private int idPaciente;
+    private String nombre;
+    private String apellido;
+    private char genero;
+    private Date fechaNacimiento;
+    private String ciudad;
+    private int idProvincia;
+    private String alergias;
+    private double peso;
+    private double altura;
+    
+
+
 }
