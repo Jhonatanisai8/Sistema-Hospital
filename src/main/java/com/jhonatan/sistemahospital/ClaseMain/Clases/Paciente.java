aa813967ef2cd2fdd3e6210faf0a3a30cad3498a
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-07-22 18:16:52 -0500

    class Paciente: constructores que nos permitan eliminar, modificar ect

--- src/main/java/com/jhonatan/sistemahospital/ClaseMain/Clases/Paciente.java
+++ src/main/java/com/jhonatan/sistemahospital/ClaseMain/Clases/Paciente.java
@@ -2,20 +2,54 @@ package com.jhonatan.sistemahospital.ClaseMain.Clases;
 
 import java.util.Date;
 
-
 public class Paciente {
-    //atributos 
+    // atributos
     private int idPaciente;
     private String nombre;
     private String apellido;
     private char genero;
     private Date fechaNacimiento;
     private String ciudad;
-    private int idProvincia;
+    private char idProvincia;
     private String alergias;
     private double peso;
     private double altura;
-    
 
+    public Paciente() {
+    }
+
+    // constructor que nos permitira eliminar
+    public Paciente(int idPaciente) {
+        this.idPaciente = idPaciente;
+    }
+
+    // constructor que nos permitira modificar
+    public Paciente(String nombre, String apellido, char genero, Date fechaNacimiento, String ciudad, char idProvincia,
+            String alergias, double peso, double altura) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.genero = genero;
+        this.fechaNacimiento = fechaNacimiento;
+        this.ciudad = ciudad;
+        this.idProvincia = idProvincia;
+        this.alergias = alergias;
+        this.peso = peso;
+        this.altura = altura;
+    }
+
+    // constructor que nos permitira eliminar
+    public Paciente(int idPaciente, String nombre, String apellido, char genero, Date fechaNacimiento, String ciudad,
+            char idProvincia, String alergias, double peso, double altura) {
+        this.idPaciente = idPaciente;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.genero = genero;
+        this.fechaNacimiento = fechaNacimiento;
+        this.ciudad = ciudad;
+        this.idProvincia = idProvincia;
+        this.alergias = alergias;
+        this.peso = peso;
+        this.altura = altura;
+    }
 
 }
