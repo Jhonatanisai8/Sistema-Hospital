88ee972af91bc0305936c35643e6690b4aab6c8f
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-07-24 18:48:47 -0500

    class Doctor: atributos

---
+++ src/main/java/com/jhonatan/sistemahospital/ClaseMain/Clases/Doctor.java
@@ -0,0 +1,12 @@
+package com.jhonatan.sistemahospital.ClaseMain.Clases;
+
+public class Doctor {
+
+    //atributos
+    private int idDoctor;
+    private String nombre;
+    private String apellido;
+    private String especialidad;
+
+    
+}
