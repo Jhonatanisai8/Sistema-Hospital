47197bb62d4c3dfc95b8e7d3a15bcddea071ac6c
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-07-24 18:51:25 -0500

    class Doctor: contructores

--- src/main/java/com/jhonatan/sistemahospital/ClaseMain/Clases/Doctor.java
+++ src/main/java/com/jhonatan/sistemahospital/ClaseMain/Clases/Doctor.java
@@ -2,11 +2,35 @@ package com.jhonatan.sistemahospital.ClaseMain.Clases;
 
 public class Doctor {
 
-    //atributos
+    // atributos
     private int idDoctor;
     private String nombre;
     private String apellido;
     private String especialidad;
+
+    // coctructores
+    public Doctor() {
+    }
+
+    // nos ayuda a eliminar
+    public Doctor(int idDoctor) {
+        this.idDoctor = idDoctor;
+    }
+
+    // nos ayuda modificar
+    public Doctor(String nombre, String apellido, String especialidad) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.especialidad = especialidad;
+    }
+
+    // nos ayuda a eliminar
+    public Doctor(int idDoctor, String nombre, String apellido, String especialidad) {
+        this.idDoctor = idDoctor;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.especialidad = especialidad;
+    }
 
     
 }
