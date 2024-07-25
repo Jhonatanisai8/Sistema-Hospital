7f8b1b3d79edbb4e71d76774831160b8ee7eecc7
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-07-24 20:15:17 -0500

    class ImpleDoctorDao implements DaoDoctor: esta clase se le daran funcionalidad a los metodo de la interface

---
+++ src/main/java/com/jhonatan/sistemahospital/DaoImplementacion/ImpleDoctorDao.java
@@ -0,0 +1,34 @@
+package com.jhonatan.sistemahospital.DaoImplementacion;
+
+import java.util.List;
+
+import com.jhonatan.sistemahospital.ClaseMain.Clases.Doctor;
+import com.jhonatan.sistemahospital.InterfacesDao.DaoDoctor;
+
+public class ImpleDoctorDao implements DaoDoctor{
+
+    @Override
+    public List<Doctor> listarDoctores() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'listarDoctores'");
+    }
+
+    @Override
+    public int insertarDoctor(Doctor doctor) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'insertarDoctor'");
+    }
+
+    @Override
+    public int modificarDoctor(Doctor doctor) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'modificarDoctor'");
+    }
+
+    @Override
+    public int eliminarDoctor(Doctor doctor) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'eliminarDoctor'");
+    }
+
+}
