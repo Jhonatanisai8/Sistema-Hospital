4313a08e6501673118ee66eec8862fd0375c2408
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-07-24 21:55:56 -0500

    class ImpleDoctorDao implements DaoDoctor: atributos, consultas y constructores

--- src/main/java/com/jhonatan/sistemahospital/DaoImplementacion/ImpleDoctorDao.java
+++ src/main/java/com/jhonatan/sistemahospital/DaoImplementacion/ImpleDoctorDao.java
@@ -1,33 +1,44 @@
 package com.jhonatan.sistemahospital.DaoImplementacion;
 
+import java.sql.*;
 import java.util.List;
 
 import com.jhonatan.sistemahospital.ClaseMain.Clases.Doctor;
+import com.jhonatan.sistemahospital.ConexionBD.Conexion;
 import com.jhonatan.sistemahospital.InterfacesDao.DaoDoctor;
 
-public class ImpleDoctorDao implements DaoDoctor{
+public class ImpleDoctorDao implements DaoDoctor {
+    private Connection conexionMYSQL;
+    Conexion instanciaMYSQL = Conexion.getInstancia();
+
+    /* consultas */
+    private static final String SQL_SELECT = "select * from doctor";
+
+    public ImpleDoctorDao() {
+
+    }
+
+    public ImpleDoctorDao(Connection connection) {
+        this.conexionMYSQL = connection;
+    }
 
     @Override
     public List<Doctor> listarDoctores() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'listarDoctores'");
+        return null;
     }
 
     @Override
     public int insertarDoctor(Doctor doctor) {
-        // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'insertarDoctor'");
     }
 
     @Override
     public int modificarDoctor(Doctor doctor) {
-        // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'modificarDoctor'");
     }
 
     @Override
     public int eliminarDoctor(Doctor doctor) {
-        // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'eliminarDoctor'");
     }
 
