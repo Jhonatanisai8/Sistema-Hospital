2660d310095b72943b9a523a9d839383eb18bf48
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-08-02 16:24:53 -0500

    clase main: haciendo visible el formulario

--- src/main/java/com/jhonatan/sistemahospital/ClaseMain/SistemaHospital.java
+++ src/main/java/com/jhonatan/sistemahospital/ClaseMain/SistemaHospital.java
@@ -1,5 +1,6 @@
 package com.jhonatan.sistemahospital.ClaseMain;
 
+import com.jhonatan.sistemahospital.ClasesEstaticas.UteleriasProvincia;
 import com.jhonatan.sistemahospital.Igu.Dashboard;
 
 public class SistemaHospital {
@@ -7,5 +8,7 @@ public class SistemaHospital {
     public static void main(String[] args) {
         Dashboard d = new Dashboard();
         d.setVisible(true);
+//        System.out.println(UteleriasProvincia.generarIdProvincia());
+
     }
 }
