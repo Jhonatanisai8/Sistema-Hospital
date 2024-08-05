76b1e9a84a2027ad1522d112bf0fe5c94b2da4b6
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-08-05 16:47:36 -0500

    clase Sistema-Hospital: llamando al formulario desde la clase main

--- src/main/java/com/jhonatan/sistemahospital/ClaseMain/SistemaHospital.java
+++ src/main/java/com/jhonatan/sistemahospital/ClaseMain/SistemaHospital.java
@@ -1,6 +1,5 @@
 package com.jhonatan.sistemahospital.ClaseMain;
 
-import com.jhonatan.sistemahospital.ClasesEstaticas.UteleriasProvincia;
 import com.jhonatan.sistemahospital.Igu.Dashboard;
 
 public class SistemaHospital {
@@ -8,7 +7,6 @@ public class SistemaHospital {
     public static void main(String[] args) {
         Dashboard d = new Dashboard();
         d.setVisible(true);
-//        System.out.println(UteleriasProvincia.generarIdProvincia());
 
     }
 }
