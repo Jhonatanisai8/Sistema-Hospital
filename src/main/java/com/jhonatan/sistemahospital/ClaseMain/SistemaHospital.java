d718b4df84a0e43851e548750a9215ee4ddea69b
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-08-13 15:28:52 -0500

    class main: método para llamar al formulario principal donde se encuentra el menu de opciones

--- src/main/java/com/jhonatan/sistemahospital/ClaseMain/SistemaHospital.java
+++ src/main/java/com/jhonatan/sistemahospital/ClaseMain/SistemaHospital.java
@@ -5,8 +5,11 @@ import com.jhonatan.sistemahospital.Igu.Dashboard;
 public class SistemaHospital {
 
     public static void main(String[] args) {
+        llamarFormularioPrincipal();
+    }
+
+    public static void llamarFormularioPrincipal() {
         Dashboard d = new Dashboard();
         d.setVisible(true);
-
     }
 }
